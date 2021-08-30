a2235c715e7907022f73fb27d288e4267c172631
KyVinE <36617949+KyVinE@users.noreply.github.com>
2021-08-30 16:25:03 -0400

    Update plots.py

--- plots.py
+++ plots.py
@@ -8,12 +8,12 @@ import os
 def allplot():
     maxx, maxy = np.loadtxt('paras', dtype=int)
     iprs = []
-    for x in range(maxx):
-        for y in range(maxy):
+    for x in range(1, maxx):
+        for y in range(1, maxy):
             cdir = os.getcwd() + '/1tun1cou{}x{}y*/inp'.format(str(x*0.01)[1:], str(y*0.01)[1:]) 
             f = glob.glob(cdir)
             iprs.append(np.average(np.loadtxt(f), axis=0)[0])
-    X, Y = np.meshgrid(list(range(maxx)), list(range(maxy)))[::-1]
+    X, Y = np.meshgrid(list(range(1, maxx)), list(range(1, maxy)))[::-1]
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     surf = ax.plot_surface(X, Y, iprs, cmap=cm.coolwarm, linewidth=0, antialiased=False)
