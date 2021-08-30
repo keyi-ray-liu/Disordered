0ce36ba06a74cce056a7fdbc12b7a7875ab26226
KyVinE <36617949+KyVinE@users.noreply.github.com>
2021-08-30 16:50:51 -0400

    Update plots.py

--- plots.py
+++ plots.py
@@ -7,13 +7,13 @@ import os
 
 def allplot():
     maxx, maxy = np.loadtxt('paras', dtype=int)
-    iprs = []
-    for x in range(1, maxx):
-        for y in range(1, maxy):
-            cdir = os.getcwd() + '/1tun1cou.{}x.{}y*/ipr'.format(str(x).zfill(2), str(y).zfill(2)) 
+    iprs = np.zeros((maxx, maxy))
+    for x in range( maxx):
+        for y in range(maxy):
+            cdir = os.getcwd() + '/1tun1cou.{}x.{}y*/ipr'.format(str(x + 1).zfill(2), str(y + 1).zfill(2)) 
             f = glob.glob(cdir)[0]
-            iprs.append(np.average(np.loadtxt(f), axis=0)[0])
-    X, Y = np.meshgrid(list(range(1, maxx)), list(range(1, maxy)))[::-1]
+            iprs[x][y] = np.average(np.loadtxt(f), axis=0)[0]
+    X, Y = np.meshgrid(list(range(1, maxx + 1)), list(range(1, maxy + 1)))[::-1]
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     surf = ax.plot_surface(X, Y, iprs, cmap=cm.coolwarm, linewidth=0, antialiased=False)
