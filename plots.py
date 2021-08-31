a9d80a39e2b6e4d018d13898064c8056f6f29a60
KyVinE <36617949+KyVinE@users.noreply.github.com>
2021-08-30 22:02:25 -0400

    Update plots.py

--- plots.py
+++ plots.py
@@ -3,16 +3,22 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import glob
 import os
+import sys
 
+def processipr():
+    maxx, maxy, eig = np.loadtxt('paras', dtype=int)
+    for i in range(eig):
+        iprs = np.zeros((maxx, maxy))
+        for x in range( maxx):
+            for y in range(maxy):
+                cdir = os.getcwd() + '/1tun1cou.{}x.{}y*/ipr'.format(str(x + 1).zfill(2), str(y + 1).zfill(2)) 
+                f = glob.glob(cdir)[0]
+                iprs[x][y] = np.average(np.loadtxt(f), axis=0)[i]
+        np.savetxt('allipr{}'.format(i), iprs)
 
-def allplot():
+def allplot(eig):
     maxx, maxy = np.loadtxt('paras', dtype=int)
-    iprs = np.zeros((maxx, maxy))
-    for x in range( maxx):
-        for y in range(maxy):
-            cdir = os.getcwd() + '/1tun1cou.{}x.{}y*/ipr'.format(str(x + 1).zfill(2), str(y + 1).zfill(2)) 
-            f = glob.glob(cdir)[0]
-            iprs[x][y] = np.average(np.loadtxt(f), axis=0)[0]
+    iprs = np.loadtxt('allipr{}'.format(eig))
     X, Y = np.meshgrid(list(range(1, maxx + 1)), list(range(1, maxy + 1)))[::-1]
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
@@ -20,5 +26,9 @@ def allplot():
     plt.show()
 
 if __name__ == '__main__':
-    allplot()
+    eig = int(sys.argv[1])
+    if os.path.exists('allipr{}'.format(eig)):
+        allplot(eig)
+    else:
+        processipr()
 
